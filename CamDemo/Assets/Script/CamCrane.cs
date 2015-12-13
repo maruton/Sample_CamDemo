d4566b75b8da04c4df40dfba48a80af2e942bf4e
Maruton Maru <marutongit@gmail.com>
2015-12-14 00:16:26 +0900

    Support leap motion. Make apk binary for android.

--- CamDemo/Assets/Script/CamCrane.cs
+++ CamDemo/Assets/Script/CamCrane.cs
@@ -14,73 +14,6 @@ using UnityEngine.UI; //need for uGUI
 using UnityEngine.EventSystems; //need for EventTrigger 
 
 /*!
- * 	@brief		Value range class <br>
- * 	@attention	None
- * 	@note		None
- * 	@author		Maruton.
- */
-public class ValueRange {// range -180 ~ 0 ~ +180
-
-	float resolution = 2f;
-	float limitPlus = 40f;
-	float limitMinus = -40f;
-	float absAdjustValue = 0f;
-
-	float resultValue = 0f;
-	public float ResultValue{
-		get{
-			resultValue = evaluateLimit(rangeValue + absAdjustValue);
-			return(resultValue);
-		}
-	}
-	public float AbsAdjustOffsetValue = 0f;
-	public float AbsAdjustValue{
-		set{absAdjustValue = value - AbsAdjustOffsetValue;}
-		get{return(absAdjustValue);}
-	}
-	float rangeValue = 0;
-	public float RangeValue{
-		set{rangeValue = value;}
-		get{return(rangeValue);}
-	}
-	public bool limitation = false;
-	public void SetResolution(float a){
-		resolution = a;
-	}
-	public void SetRange(float minus, float plus){
-		limitMinus = minus; limitPlus = plus;
-	}
-	public void Add(){
-		rangeValue += resolution;
-		if(rangeValue>180f) rangeValue = -180f + (rangeValue - 180f);
-		rangeValue = evaluateLimit(rangeValue);
-	}
-	public void Sub(){
-		rangeValue -= resolution;
-		if(rangeValue<-180f) rangeValue = 180f + (rangeValue + 180f);
-		rangeValue = evaluateLimit(rangeValue);
-	}
-	float evaluateLimit(float a){
-		if(limitation){
-			if(a<=limitMinus) a = limitMinus;
-			if(a>=limitPlus) a = limitPlus;
-		}
-		return(a);
-	}
-	float adj180(float a){
-		if(a>180f){
-			while(a>180f) a-= 180f;
-			a = -180f + a;
-		}
-		else if(a<-180f){
-			while(a<-180f) a+= 180f;
-			a = 180f - a;
-		}
-		return(a);
-	}
-}
-
-/*!
  * 	@brief		CamCrane class <br>
  * 	@attention	None
  * 	@note		None
@@ -123,53 +56,84 @@ public class CamCrane : MonoBehaviour {
 	}
 
 
+	
+	ValueRange turn;// = new ValueRange(); //!< Control to range for turn.
+	ValueRange trim;// = new ValueRange();	//!< Control to range for trim.
+	ValueRange zoom;// = new ValueRange(); //!< Control to range for zoom.
+
+	CalcDiffDegree turnDiffDeg;
+	CalcDiffDegree trimDiffDeg;
+
+	uGUI_Accel accelGui;
+
+	Text Txt_AccelValue;
+
 	/*!	Initial procedure.
 	 * 	Initial class for degree. 
    	 * 	@note		 	None
    	 * 	@attention		None 
    	 */	
 	void Init(){
+
+		GameObject go = GameObject.Find("Canvas/Text_AccelValue");
+		Txt_AccelValue = go.GetComponent<Text>();
+
+		accelGui = GetComponent<uGUI_Accel>();
+
+		go_CamHorizontal = GameObject.Find("CamHorizontal");
+		go_CamVertical = GameObject.Find("CamVertical");
+		go_CamZoom = GameObject.Find("CamZoom");
+		
+		turn = gameObject.AddComponent<ValueRange>();
+		trim = gameObject.AddComponent<ValueRange>();
+		zoom = gameObject.AddComponent<ValueRange>();
+
+		trimDiffDeg = go_CamVertical.AddComponent<CalcDiffDegree>();
+		turnDiffDeg = go_CamHorizontal.AddComponent<CalcDiffDegree>();
+
 		turn.RangeValue = 0f;
 		turn.SetResolution(2f);		//Moving step
-		turn.SetRange(-80f, 80f);	//Left,Right
-		turn.limitation = true;		//Limit sw
+		turn.SetRange(-80f, 80f);	//Degree range Left,Right
+		turn.limitation = true;		//Limit range sw
 		turn.AbsAdjustOffsetValue = 0f;//Absolute offset for Accel sensor
 
 		trim.RangeValue = 0f;
 		trim.SetResolution(2f);		//Moving step
-		trim.SetRange(-10f, 35f);	//Down,Up
+		trim.SetRange(-10f, 35f);	//Degree range Down,Up
 		trim.limitation = true;		//Limit sw
 		trim.AbsAdjustOffsetValue = 20f;//Absolute offset for Accel sensor
 
 		zoom.RangeValue = 3.3f;
 		zoom.SetResolution(0.05f);	//Moving step
 		zoom.SetRange(1.6f, 5f);	//Zoom up, Zoom down
-		zoom.limitation = true;		//Limit sw
+		zoom.limitation = true;		//Limit range sw
 
 		HorizontalLeapSourceRot = HorizontalBaseRot;
 		HorizontalLeapTargetRot = HorizontalBaseRot;
 		HorizontalLeapResultRot = HorizontalBaseRot;
+
+		VerticalLeapSourceRot = VerticalBaseRot;
+		VerticalLeapTargetRot = VerticalBaseRot;
+		VerticalLeapResultRot = VerticalBaseRot;
+
 	}
-	ValueRange zoom = new ValueRange(); //!< Control to range for zoom.
-	ValueRange turn = new ValueRange(); //!< Control to range for turn.
-	ValueRange trim = new ValueRange();	//!< Control to range for trim.
+	//--------------------
 	Quaternion HorizontalBaseRot = Quaternion.Euler(0,0,90);//!< Model offset for model.
 	Quaternion HorizontalOffsetRot = Quaternion.identity;	//!< Turn offset for moving.
 	Quaternion VerticalBaseRot = Quaternion.Euler(0,0,0);	//!< Trim offset for model.
 	Quaternion VerticalOffsetRot = Quaternion.identity;		//!< Trim offset for moving.
+	//--------------------
 	void update_Zoom(){
 		//Debug.Log (zoom.RangeValue);
 		go_CamZoom.transform.localPosition = new Vector3(0,0,zoom.RangeValue + AccelZoom);
 		
 	}
-
-	public bool enableLeap_Turn = true;
+	//--------------------
+	public bool EnableLeap_Turn = true;
 	void update_Turn(){
-		//Debug.Log (turn.RangeValue);
-		turn.AbsAdjustValue = AccelX;
+		turn.AbsAdjustValue = Detected_acceleration_DegX;
 		HorizontalOffsetRot = Quaternion.Euler(turn.ResultValue,0,0);
-		if(enableLeap_Turn){
-			HorizontalLeapTargetRot = HorizontalBaseRot * HorizontalOffsetRot;
+		if(EnableLeap_Turn){
 			Update_TurnLeap();
 		}
 		else{
@@ -181,27 +145,53 @@ public class CamCrane : MonoBehaviour {
 	Quaternion HorizontalLeapTargetRot;
 	Quaternion HorizontalLeapResultRot;
 	float Turn_LapTime = 0f;
-	float Turn_Time = 0.3f;
+	float Turn_Time = 0f;
+	int Mode_TurnLeap = 0;
+	public float Mag_TurnLeapDelta = 1.0f;// (0.01~1.0)  0.2=smooth & slow.0.4,0.6,1.0
 	void Update_TurnLeap(){
-		Turn_LapTime += Time.deltaTime;// add frame time.
-		if(Turn_LapTime<Turn_Time){
-			HorizontalLeapResultRot = Quaternion.Lerp(HorizontalLeapSourceRot, HorizontalLeapTargetRot, Turn_LapTime/Turn_Time);
-			//HorizontalLeapSourceRot = HorizontalLeapResultRot;
-		}
-		else{
-			HorizontalLeapResultRot = HorizontalLeapTargetRot;
+		Mag_TurnLeapDelta = accelGui.ParamH;
+		if(Detected_acceleration_X){
+			Detected_acceleration_X = false;
+			Quaternion currentRot = go_CamHorizontal.transform.localRotation;//Check Local?Global?
+			//Debug.Log ("currentRot = "+currentRot.ToString("F5"));
+			HorizontalLeapTargetRot = HorizontalBaseRot * HorizontalOffsetRot;
+			HorizontalLeapSourceRot = currentRot;
+			Turn_Time = (Quaternion.Angle(HorizontalLeapTargetRot , HorizontalLeapSourceRot))/180.0f;
 			Turn_LapTime = 0f;
-			HorizontalLeapSourceRot = HorizontalLeapResultRot;
+			Mode_TurnLeap = 1;
+			/*Debug.Log ("0 : HorizontalLeapTargetRot="+HorizontalLeapTargetRot.ToString("F5")+
+			           "HorizontalLeapSourceRot="+HorizontalLeapSourceRot.ToString("F5")+
+			           "  ("+Turn_LapTime.ToString("F5")+"/"+Turn_Time.ToString("F5")+")  "
+			           );*/
+		}
+		if(Mode_TurnLeap==1){
+			float t = (Time.deltaTime * Mag_TurnLeapDelta);
+			if(t==0.0f) t = 0.005f;//Safe code.
+			Turn_LapTime += t;
+			if(Turn_LapTime<Turn_Time){//Leap moving.
+				HorizontalLeapResultRot = Quaternion.Lerp(HorizontalLeapSourceRot, HorizontalLeapTargetRot, Turn_LapTime/Turn_Time);
+				/*Debug.Log ("1a: HorizontalLeapTargetRot="+HorizontalLeapTargetRot.ToString("F5")+
+				           "HorizontalLeapSourceRot="+HorizontalLeapSourceRot.ToString("F5")+
+				           "  ("+Turn_LapTime.ToString("F5")+"/"+Turn_Time.ToString("F5")+")  "
+				           );*/
+			}
+			else{//Leap move end.
+				HorizontalLeapResultRot = HorizontalLeapTargetRot;
+				Turn_LapTime = 0f;
+				/*Debug.Log ("0e: HorizontalLeapTargetRot="+HorizontalLeapTargetRot.ToString("F5")+
+				           "HorizontalLeapSourceRot="+HorizontalLeapSourceRot.ToString("F5")+
+				           "  ("+Turn_LapTime.ToString("F5")+"/"+Turn_Time.ToString("F5")+")  "
+				           );*/
+				Mode_TurnLeap = 0;
+			}
 		}
 	}
-	public bool enableLeap_Trim = true;
+	//--------------------
+	public bool EnableLeap_Trim = true;
 	void update_Trim(){
-		//Debug.Log (trim.RangeValue);
-		//Debug.Log (trim.ResultValue);
-		trim.AbsAdjustValue = AccelY;
+		trim.AbsAdjustValue = Detected_acceleration_DegY;
 		VerticalOffsetRot = Quaternion.Euler(0,trim.ResultValue,0);
-		if(enableLeap_Trim){
-			VerticalLeapTargetRot = VerticalBaseRot * VerticalOffsetRot;
+		if(EnableLeap_Trim){
 			Update_TrimLeap();
 		}
 		else{
@@ -209,136 +199,74 @@ public class CamCrane : MonoBehaviour {
 		}
 		go_CamVertical.transform.localRotation = VerticalLeapResultRot;
 	}
-	Quaternion VerticalLeapSourceRot = Quaternion.identity;
-	Quaternion VerticalLeapTargetRot = Quaternion.identity;
-	Quaternion VerticalLeapResultRot = Quaternion.identity;
+	Quaternion VerticalLeapSourceRot;
+	Quaternion VerticalLeapTargetRot;
+	Quaternion VerticalLeapResultRot;
 	float Trim_LapTime = 0f;
-	float Trim_Time = 0.3f;
+	float Trim_Time = 0f;
+	int Mode_TrimLeap = 0;
+	public float Mag_TrimLeapDelta = 1.0f;// (0.01~1.0)  10.0f=smooth & slow.
 	void Update_TrimLeap(){
-		Trim_LapTime += Time.deltaTime;// add frame time.
-		if(Trim_LapTime<Trim_Time){
-			VerticalLeapResultRot = Quaternion.Lerp(VerticalLeapSourceRot, VerticalLeapTargetRot, Trim_LapTime/Trim_Time);
-			//VerticalLeapSourceRot = VerticalLeapResultRot;
-		}
-		else{
-			VerticalLeapResultRot = VerticalLeapTargetRot;
-			Trim_LapTime = 0f;
-			VerticalLeapSourceRot = VerticalLeapResultRot;
-		}
-	}
-	//
+		Mag_TrimLeapDelta = accelGui.ParamV;
+		switch(Mode_TrimLeap){
+		case 0://Leap start
+			if(Detected_acceleration_Y){
+				Detected_acceleration_Y = false;
+				Quaternion currentRot = go_CamVertical.transform.localRotation;
+				//Debug.Log ("currentRot = "+currentRot.ToString("F5"));
+				VerticalLeapTargetRot = VerticalBaseRot * VerticalOffsetRot;
+				VerticalLeapSourceRot = currentRot;
+				Trim_Time = (Quaternion.Angle(VerticalLeapTargetRot , VerticalLeapSourceRot))/180.0f;
+				Trim_LapTime = 0f;
+				Mode_TrimLeap = 1;
+				/*Debug.Log ("0 : VerticalLeapTargetRot="+VerticalLeapTargetRot.ToString("F5")+
+				           "VerticalLeapSourceRot="+VerticalLeapSourceRot.ToString("F5")+
+				           "  ("+Trim_LapTime.ToString("F5")+"/"+Trim_Time.ToString("F5")+")  "
+				           );*/
+			}
+			break;
+		case 1://<Leap moving
+			//float t = Time.deltaTime;
+			float t = (Time.deltaTime * Mag_TrimLeapDelta);
+			if(t==0.0f) t = 0.005f;//Safe code.
+			Trim_LapTime += t;
+			if(Trim_LapTime<Trim_Time){//Leap moving.
+				VerticalLeapResultRot = Quaternion.Lerp(VerticalLeapSourceRot, VerticalLeapTargetRot, Trim_LapTime/Trim_Time);
+				/*Debug.Log ("1a: VerticalLeapTargetRot="+VerticalLeapTargetRot.ToString("F5")+
+				           "VerticalLeapSourceRot="+VerticalLeapSourceRot.ToString("F5")+
+				           "  ("+Trim_LapTime.ToString("F5")+"/"+Trim_Time.ToString("F5")+")  "
+				           );*/
+			}
+			else{//Leap move end.
+				VerticalLeapResultRot = VerticalLeapTargetRot;
+				Trim_LapTime = 0f;
+				/*Debug.Log ("0e: VerticalLeapTargetRot="+VerticalLeapTargetRot.ToString("F5")+
+				           "VerticalLeapSourceRot="+VerticalLeapSourceRot.ToString("F5")+
+				           "  ("+Trim_LapTime.ToString("F5")+"/"+Trim_Time.ToString("F5")+")  "
+				           );*/
+				Mode_TrimLeap = 0;
+			}
+			break;
 
-	/*!	Turn right camera.
-	 *	Simply turn right camera. 
-   	 * 	@note		 	None
-   	 * 	@attention		None 
-   	 */	
-	void Entry_Turn_Right(){
-		turn.Sub();
-	}
-
-	/*!	Turn left camera.
-	 *	Simply turn left camera. 
-   	 * 	@note		 	None
-   	 * 	@attention		None 
-   	 */	
-	void Entry_Turn_Left(){
-		turn.Add();
-	}
-
-	const float VerticalStep = 2f; //!< Resolution for camera lookup and lookdown.
-	Quaternion rotVertical = Quaternion.identity;
-	/*!	Lookup camera.
-	 *	Simply lookup camera. 
-   	 * 	@note		 	None
-   	 * 	@attention		None 
-   	 */	
-	void Entry_LookUp(){
-		trim.Sub();
-	}
-
-	/*!	Lookdown camera.
-	 *	Simply lookdown camera. 
-   	 * 	@note		 	None
-   	 * 	@attention		None 
-   	 */	
-	void Entry_LookDown(){
-		trim.Add();
-	}
-	//-----
-	GameObject go_CamHorizontal;  //!< Object for turn Horizontal.
-	GameObject go_CamVertical; //!< Object for turn vertical.
-	GameObject go_CamZoom;	   //!< Object for zooming.
-	GameObject go_Text_AccelValue;	   //!< Object for zooming.
-	Text Txt_AccelValue;
-
-	//-----
-	void Awake(){
-		go_CamHorizontal = GameObject.Find("CamHorizontal");
-		go_CamVertical = GameObject.Find("CamVertical");
-		go_CamZoom = GameObject.Find("CamZoom");
-
-		GameObject go;
-		go = GameObject.Find("Canvas/Text_AccelValue");
-		Txt_AccelValue = go.GetComponent<Text>();
-
-		Init();
-
-	}
-	void Start () {
-	
-	}
-
-	/*!	Keyboard operation.
-	 *	Input for PC's keyboard.
-   	 * 	@note		 	None
-   	 * 	@attention		None 
-   	 */	
-	void KeyOperation(){
-		if(Input.GetKey(KeyCode.D)){
-			Entry_Turn_Right();
-		}
-		else if(Input.GetKey(KeyCode.A)){
-			Entry_Turn_Left();
-		}
-
-		if(Input.GetKey(KeyCode.W)){
-			Entry_LookDown();
-		}
-		else if(Input.GetKey(KeyCode.S)){
-			Entry_LookUp();
-		}
-
-		if(Input.GetKey(KeyCode.PageUp)){
-			Entry_ZoomUp();
-		}
-		else if(Input.GetKey(KeyCode.PageDown)){
-			Entry_ZoomDown();
+		default:
+			break;
 		}
 	}
-
-	//---
-	const float SensorXLimit = 0.5f;
-	const float SensorYLimit = 0.5f;
-	public float AccelX = 0f;		public float AccelY = 0f;
-	float Raw_AccelX = 0;			float Raw_AccelY = 0;
-	float Raw_Before_AccelX = 0;	float Raw_Before_AccelY = 0;
+	//--------------------
 	const float Mag_AccelX = 80f;	const float Mag_AccelY = 80f;
-	//Quaternion HorizontalAccelRot = Quaternion.identity;
-	//Quaternion VerticalAccelRot = Quaternion.identity;
-
-	const float LimitVertical = 0.5f;
-
-	const float LimitZoom = 0.5f;
 	float AccelZoom = 0;
 	const float Mag_AccelZoom = 1f;
 
-	Vector3 before_acceleration = new Vector3(0,0,0);
-	int chatterX = 0;	int before_direcX = 0;	int direcX = 0;
-	int chatterY = 0;	int before_direcY = 0;	int direcY = 0;
-	public int ChatterX = 30; 
-	public int ChatterY = 30;
+	Vector3 Last_acceleration = new Vector3(0,0,0);
+	bool Detected_acceleration_X = false;
+	bool Detected_acceleration_Y = false;
+	public float Detected_acceleration_DegX = 0f;
+	public float Detected_acceleration_DegY = 0f;
+	//public float AccelX = 0f;
+	//public float AccelY = 0f;
+
 	public bool EnableAccelSensor = true;//!< Enable accel sensor
+	public bool EnableAccelSensor_DetectDiffrencial = true;//!< Enable accel sensor motion detect.
 	/*!	Accelerate operation.
 	 *	Accelerate operation.
    	 * 	@note		 	None
@@ -349,54 +277,40 @@ public class CamCrane : MonoBehaviour {
 		Txt_AccelValue.text = acceleration.ToString("F4");
 		if(!EnableAccelSensor){
 			acceleration = Vector3.zero;
-			AccelX = AccelY =0f;
+			Detected_acceleration_DegX = Detected_acceleration_DegY = 0f;
 			return;
 		}
 		//Debug.Log (acceleration);
 		float diff;
 		//Debug.Log (acceleration);
 
-		bool enableX = false;
-		bool enableY = false;
+		bool accelEnableX = false;
+		bool accelEnableY = false;
 
-		//Exclusion hand jitter X
-		if(acceleration.x>before_acceleration.x) direcX = 1;
-		else if(acceleration.x>before_acceleration.x) direcX = -1;
-		else direcX = 0;
-		if(before_direcX==direcX) chatterX++;
-		if(chatterX>ChatterX) enableX = true;// Adjustable feel for exclusion hand jitter.(1/30frame*n)
-
-		//Exclusion hand jitter Y
-		if(acceleration.y>before_acceleration.y) direcY = 1;
-		else if(acceleration.y>before_acceleration.y) direcY = -1;
-		else direcY = 0;
-		if(before_direcX==direcY) chatterY++;
-		if(chatterY>ChatterY) enableY = true;// Adjustable feel for exclusion hand jitter.(1/30frame*n)
-
-
-		if(enableX){
-			Raw_AccelX = acceleration.x;
-			diff = Raw_Before_AccelX - Raw_AccelX;
-			//if(acceleration.x>SensorXLimit) 		Entry_Turn_Left();
-			//else if(acceleration.x<-SensorXLimit) 	Entry_Turn_Right();
-			if(Mathf.Abs(diff)>0.015f) {
-				AccelX = acceleration.x * Mag_AccelX; //Exclision Chatter
-				//HorizontalAccelRot = Quaternion.Euler(new Vector3(AccelX, 0, 0));
+		if(EnableAccelSensor_DetectDiffrencial){
+			//Moved abit far distance that must be move now.
+			if(!accelEnableX){
+				diff = Last_acceleration.x - acceleration.x;
+				//Debug.Log ("diff="+diff.ToString("F5")+"acceleration.x="+acceleration.x.ToString("F5")+"  before_acceleration.x="+before_acceleration.x.ToString("F5")  );
+				if(Mathf.Abs(diff)>0.015f) accelEnableX = true;
 			}
-			Raw_Before_AccelX = Raw_AccelX;
-		}
-		if(enableY){
-			Raw_AccelY = acceleration.y;
-			diff = Raw_Before_AccelY - Raw_AccelY;
-			//if(acceleration.y>SensorYLimit) 		Entry_LookUp();
-			//else if(acceleration.y<-SensorYLimit) 	Entry_LookDown();
-			if(Mathf.Abs(diff)>0.015f) {
-				AccelY = acceleration.y * -Mag_AccelY; //Exclision Chatter
-				//VerticalAccelRot = Quaternion.Euler(new Vector3(0, AccelY, 0));
+			//Moved abit far distance that must be move now.
+			if(!accelEnableY){
+				diff = Last_acceleration.y - acceleration.y;
+				//Debug.Log ("diff="+diff.ToString("F5")+"acceleration.y="+acceleration.y.ToString("F5") );
+				if(Mathf.Abs(diff)>0.015f) accelEnableY = true;
 			}
-			Raw_Before_AccelY = Raw_AccelY;
 		}
-		before_acceleration = acceleration;
+		if(accelEnableX){
+			Detected_acceleration_DegX = acceleration.x * Mag_AccelX; //Exclision Chatter
+			Last_acceleration.x = acceleration.x;
+			Detected_acceleration_X = true;
+		}
+		if(accelEnableY){
+			Detected_acceleration_DegY = acceleration.y * -Mag_AccelY; //Exclision Chatter
+			Last_acceleration.y = acceleration.y;
+			Detected_acceleration_Y = true;
+		}
 	}
 
 	void Update () {
@@ -418,6 +332,87 @@ public class CamCrane : MonoBehaviour {
 			Application.Quit();
 		}
 		#endif
+	}
+	//-----
+	GameObject go_CamHorizontal;  //!< Object for turn Horizontal.
+	GameObject go_CamVertical; //!< Object for turn vertical.
+	GameObject go_CamZoom;	   //!< Object for zooming.
+	
+	//-----
+	void Awake(){
+		Init();
+		
+	}
+	void Start () {
+
+		Quaternion a = new Quaternion(0f, 0.20773f, 0f, 0.97819f);
+		Quaternion b = new Quaternion(0f, 0.00170f, 0f, 1.0f);
+		float t = Quaternion.Angle(a , b);
+		//Debug.Log("t="+t);
+	}
+	
+	/*!	Turn right camera.
+	 *	Simply turn right camera. 
+   	 * 	@note		 	None
+   	 * 	@attention		None 
+   	 */	
+	void Entry_Turn_Right(){
+		turn.Sub();
+	}
+	
+	/*!	Turn left camera.
+	 *	Simply turn left camera. 
+   	 * 	@note		 	None
+   	 * 	@attention		None 
+   	 */	
+	void Entry_Turn_Left(){
+		turn.Add();
+	}
+	
+	/*!	Lookup camera.
+	 *	Simply lookup camera. 
+   	 * 	@note		 	None
+   	 * 	@attention		None 
+   	 */	
+	void Entry_LookUp(){
+		trim.Sub();
+	}
+	
+	/*!	Lookdown camera.
+	 *	Simply lookdown camera. 
+   	 * 	@note		 	None
+   	 * 	@attention		None 
+   	 */	
+	void Entry_LookDown(){
+		trim.Add();
+	}
+
+	/*!	Keyboard operation.
+	 *	Input for PC's keyboard.
+   	 * 	@note		 	None
+   	 * 	@attention		None 
+   	 */	
+	void KeyOperation(){
+		if(Input.GetKey(KeyCode.D)){
+			Entry_Turn_Right();
+		}
+		else if(Input.GetKey(KeyCode.A)){
+			Entry_Turn_Left();
+		}
+		
+		if(Input.GetKey(KeyCode.W)){
+			Entry_LookDown();
+		}
+		else if(Input.GetKey(KeyCode.S)){
+			Entry_LookUp();
+		}
+		
+		if(Input.GetKey(KeyCode.PageUp)){
+			Entry_ZoomUp();
+		}
+		else if(Input.GetKey(KeyCode.PageDown)){
+			Entry_ZoomDown();
+		}
 	}
 }
 
