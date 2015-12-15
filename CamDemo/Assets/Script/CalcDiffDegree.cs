2998fd0ad7e5c8787f12abf0d877f8cfcb48ea10
Maruton Maru <marutongit@gmail.com>
2015-12-15 21:44:19 +0900

    Add new files that forget to commit(oops!).

---
+++ CamDemo/Assets/Script/CalcDiffDegree.cs
@@ -0,0 +1,47 @@
+﻿/*!
+ * 	@file		CalcDifferenceDegree.cs 
+ * 	@brief		'CalcDifferenceDegree' Class source file. 
+ * 	@note		None 
+ *	@attention 
+ *  			[CalcDifferenceDegree.cs]							<br>
+ *				Copyright (c) [2015] [Maruton] 						<br>
+ *				This software is released under the MIT License. 	<br>
+ *				http://opensource.org/licenses/mit-license.php 		<br>
+ */
+using UnityEngine;
+using System.Collections;
+
+public class CalcDiffDegree : MonoBehaviour {
+	public GameObject go_SourceUnit;
+	public GameObject go_TargetUnity;
+	
+	void Start () {
+	}
+	
+	public float DeltaDegree(GameObject go_src, GameObject go_dst){
+		Vector3 srcPos = go_src.transform.position;
+		Vector3 dstPos = go_dst.transform.position;
+		
+		Vector3 relativeDegree = (dstPos - srcPos).normalized;
+		//Debug.Log ("srcPos="+srcPos+" dstPos="+dstPos+" relativeDegree="+relativeDegree);
+		Vector3 srcforward = transform.forward;//Direction forward src.
+		float angle = Vector3.Angle(relativeDegree , srcforward);
+		//Debug.Log ("angle="+angle+" srcforward="+srcforward);
+		return(angle);
+	}
+	public float DeltaDegree(Vector3 srcPos, Vector3 dstPos){
+		Vector3 relativeDegree = (dstPos - srcPos).normalized;
+		//Debug.Log ("srcPos="+srcPos+" dstPos="+dstPos+" relativeDegree="+relativeDegree);
+		Vector3 srcforward = transform.forward;//Direction forward src.
+		float angle = Vector3.Angle(relativeDegree , srcforward);
+		//Debug.Log ("angle="+angle+" srcforward="+srcforward);
+		return(angle);
+	}
+	public float DeltaDegree(Quaternion srcQua, Quaternion dstQua){
+		return(DeltaDegree(srcQua.eulerAngles, dstQua.eulerAngles));
+	}
+
+	//void Update () {
+	//	float deltaAngle = DeltaDegree(go_SourceUnit, go_TargetUnity);
+	//}
+}
---
+++ CamDemo/Assets/Script/ValueRange.cs
@@ -0,0 +1,92 @@
+﻿/*!
+ * 	@file		ValueRange.cs 
+ * 	@brief		'ValueRange' Class source file. 
+ * 	@note		None 
+ *	@attention 
+ *  			[ValueRange.cs]										<br>
+ *				Copyright (c) [2015] [Maruton] 						<br>
+ *				This software is released under the MIT License. 	<br>
+ *				http://opensource.org/licenses/mit-license.php 		<br>
+ */
+
+using UnityEngine;
+using System.Collections;
+
+/*!
+ * 	@brief		Value range class <br>
+ * 	@attention	None
+ * 	@note		None
+ * 	@author		Maruton.
+ */
+public class ValueRange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	float resolution = 2f;
+	float limitPlus = 40f;
+	float limitMinus = -40f;
+	float absAdjustValue = 0f;
+	
+	float resultValue = 0f;
+	public float ResultValue{
+		get{
+			resultValue = evaluateLimit(rangeValue + absAdjustValue);
+			return(resultValue);
+		}
+	}
+	float absAdjustOffsetValue = 0f;
+	public float AbsAdjustOffsetValue{
+		set{absAdjustOffsetValue = value;}
+		get{return(absAdjustOffsetValue);}
+	}
+	public float AbsAdjustValue{
+		set{absAdjustValue = value - AbsAdjustOffsetValue;}
+		get{return(absAdjustValue);}
+	}
+	float rangeValue = 0;
+	public float RangeValue{
+		set{rangeValue = value;}
+		get{return(rangeValue);}
+	}
+	public bool limitation = false;
+	public void SetResolution(float a){
+		resolution = a;
+	}
+	public void SetRange(float minus, float plus){
+		limitMinus = minus; limitPlus = plus;
+	}
+	public void Add(){
+		rangeValue += resolution;
+		if(rangeValue>180f) rangeValue = -180f + (rangeValue - 180f);
+		rangeValue = evaluateLimit(rangeValue);
+	}
+	public void Sub(){
+		rangeValue -= resolution;
+		if(rangeValue<-180f) rangeValue = 180f + (rangeValue + 180f);
+		rangeValue = evaluateLimit(rangeValue);
+	}
+	float evaluateLimit(float a){
+		if(limitation){
+			if(a<=limitMinus) a = limitMinus;
+			if(a>=limitPlus) a = limitPlus;
+		}
+		return(a);
+	}
+	float adj180(float a){
+		if(a>180f){
+			while(a>180f) a-= 180f;
+			a = -180f + a;
+		}
+		else if(a<-180f){
+			while(a<-180f) a+= 180f;
+			a = 180f - a;
+		}
+		return(a);
+	}
+}
